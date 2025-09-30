a9a4b055103f7c76472bb085fe5c19624400aa39
cassius <cassiustedesco1@gmail.com>
2025-10-01 00:21:44 +0200

    Implemented getInventory and writeInventory in DaoImplFile #2

---
+++ src/dao/DaoImplFile.java
@@ -0,0 +1,150 @@
+package dao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import model.Amount;
+import model.Employee;
+import model.Product;
+
+public class DaoImplFile implements Dao {
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		ArrayList<Product> inventory = new ArrayList<Product>();
+		File f = new File(System.getProperty("user.dir") + File.separator + "files/inputInventory.txt");
+		
+		try {			
+			// wrap in proper classes
+			FileReader fr;
+			fr = new FileReader(f);				
+			BufferedReader br = new BufferedReader(fr);
+			
+			// read first line
+			String line = br.readLine();
+			
+			// process and read next line until end of file
+			while (line != null) {
+				// split in sections
+				String[] sections = line.split(";");
+				
+				String name = "";
+				double wholesalerPrice=0.0;
+				int stock = 0;
+				
+				// read each sections
+				for (int i = 0; i < sections.length; i++) {
+					// split data in key(0) and value(1) 
+					String[] data = sections[i].split(":");
+					
+					switch (i) {
+					case 0:
+						// format product name
+						name = data[1];
+						break;
+						
+					case 1:
+						// format price
+						wholesalerPrice = Double.parseDouble(data[1]);
+						break;
+						
+					case 2:
+						// format stock
+						stock = Integer.parseInt(data[1]);
+						break;
+						
+					default:
+						break;
+					}
+				}
+				// add product to inventory
+				inventory.add(new Product(name, new Amount(wholesalerPrice), true, stock));
+				
+				// read next line
+				line = br.readLine();
+			}
+			fr.close();
+			br.close();
+			return inventory;
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return inventory;
+	}
+
+	@Override
+	public boolean writeInventory(ArrayList<Product> products) {
+		LocalDate myObj = LocalDate.now();
+		String fileName = "inventory_" + myObj.toString() + ".txt";
+		
+		// locate file, path and name
+		File f = new File(System.getProperty("user.dir") + File.separator + "files" + File.separator + fileName);
+				
+		try {
+			// wrap in proper classes
+			FileWriter fw;
+			fw = new FileWriter(f, true);
+			PrintWriter pw = new PrintWriter(fw);
+			
+			// write line by line
+			int productId=1;
+			for (Product product : products) {
+				
+				// Line = 1;Product:Manzana;Stock:10;
+				
+//				2;Product:Fresa;Stock:20;
+//				Total number of products:2;
+
+				// format first line TO BE -> 1;Product:Manzana;Stock:10;
+				StringBuilder line = new StringBuilder(productId+";Product:"+product.getName()+";Stock:"+product.getStock()+";");
+				pw.write(line.toString());
+				fw.write("\n");
+				
+				// increment counter sales
+				productId++;
+			}
+				pw.write("Total number of products:"+products.size()+";");
+				fw.write("\n");
+
+			// close files
+			pw.close();
+			fw.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;			
+		}
+	}
+
+	@Override
+	public Employee getEmployee(int employeeId, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
